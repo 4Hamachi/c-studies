14427e62058969215433afe357da60e537943614
Hamachi <lordvertrigo@gmail.com>
2019-06-10 20:57:04 -0300

    first class

---
+++ code.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+ 
+    int numero1 = 1;
+    int numero2 = 2;
+    int numero3 = 3;
+    int numero4 = 4;
+
+    printf("meuprograma c\n");
+
+    return 0;
+
+
+}
