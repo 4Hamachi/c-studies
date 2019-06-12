5ce10cc29e7907e3e5187357b695015717ecd196
Hamachi <lordvertrigo@gmail.com>
2019-06-12 20:36:32 -0300

    code.c

--- code.c
+++ code.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
- 
+    float f;
+    int nro;
     int numero1 = 1;
     int numero2 = 2;
     int numero3 = 3;
     int numero4 = 4;
 
-    printf("meuprograma c\n");
-
+    printf("digite dois numero: ");
+    scanf("%d%f",&nro,&f);
+    printf("numeros: %d e %f\n",nro,f);
+    
+    
     return 0;
+
 
 
 }
