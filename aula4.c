dde4b1691eac464a480f1ba57ddc170a708656fc
Hamachi <lordvertrigo@gmail.com>
2019-06-13 19:05:11 -0300

    aula4.c

---
+++ aula4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int main(){
+        int x = 10, y = 6;
+
+        printf("resultado = %d\n",x >= y+4);
+        printf("resultado = %d\n", x > 4);
+        printf("resultado = %d\n", x < y);
+        printf("resultado = %d\n", x == 4);
+        printf("resultado = %d\n", x-4 != y);
+        printf("resultado = %d\n", x < 4);
+    
+    return 0;
+
+    } 
