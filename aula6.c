1cf04df145c1bd6168ea9a191d8e52569ff2a551
Hamachi <lordvertrigo@gmail.com>
2019-06-14 18:42:02 -0300

    aula6.c

---
+++ aula6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int x;
+    printf("digite um valor inteiro: ");
+    scanf("%d",&x);
+    if (x%2 == 0)
+        printf("o valor eh par.\n");
+        else
+        printf("o valor eh impar. \n");
+
+        printf("o valor eh impar. \n");
+        printf("fim do progama");
+        return 0;        
+    
+        
+        
+}
