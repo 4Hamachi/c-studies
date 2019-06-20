9ab8927ddfae4f1b50830ee74843a12b44e489ca
Hamachi <lordvertrigo@gmail.com>
2019-06-20 17:50:20 -0300

    trabalho.c

---
+++ Works/firstExercices/DobroNumero/trabalho.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int nro;
+    int x;
+
+    printf("digite um numero: ");
+    scanf("%d",&nro); 
+    x = nro *2 ;
+    printf("o dobro do numero: %d \n",x);
+    
+    return 0;
+}
