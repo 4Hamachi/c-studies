21b10d024266c703102438ad91cd02fd143ec275
Hamachi <lordvertrigo@gmail.com>
2019-08-01 20:59:16 -0300

    fazendo trabalho

---
+++ 16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+
+    printf("digite o volor de a: ");
+    scanf("%d",&a);
+    printf("digite o valor de b: ");
+    scanf("%d",&b);
+
+    while(a <=  b)
+    {
+        a = a + 1;
+        //if(a == 5)
+            continue;
+            printf("%d\n",a);
+
+    }
+    return 0;
+}
